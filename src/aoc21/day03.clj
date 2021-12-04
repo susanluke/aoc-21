c3ee0afed8607e3b00f50b856a22182eff0659d6
Susan Luke <susan_luke@mail.com>
2021-12-04 18:03:20 +0000

    Day 3, needs a bit of tidying3

--- src/aoc21/day03.clj
+++ src/aoc21/day03.clj
@@ -4,28 +4,54 @@
             [clojure.core.matrix :as m]))
 
 (def f "day-03-input.txt")
+(def numeric-offset 48)
 
 (defn parse-file []
   (->> (-> f io/resource slurp string/split-lines vec)
        (mapv #(mapv (fn [c] (Character/digit c 2)) %))))
 
-(defn binary-char-vec->int [cs]
-  (->> cs
-       (apply str)
-       (str "2r")
-       read-string))
+(defn binary-vec->int [cs]
+  (reduce #(+ (* 2 %) %2) cs))
 
-(defn get-day03-answer-pt1 []
-  (let [data       (parse-file)
-        num-lines  (count data)
+(defn get-gamma-epsilon-bits [data]
+  (let [num-lines  (count data)
         threshold  (/ num-lines 2)
         bit-counts (->> data
                         m/transpose
-                        (m/slice-map #(apply + %)))
-        epsilon    (->> bit-counts
-                        (map #(if (< threshold %) \1 \0))
-                        binary-char-vec->int)
-        gamma      (->> bit-counts
-                        (map #(if (> threshold %) \1 \0))
-                        binary-char-vec->int)]
-    (* gamma epsilon)))
+                        (m/slice-map m/esum))
+        epsilon    (map #(if (< threshold %) 1 0) bit-counts)
+        gamma      (map #(if (> threshold %) 1 0) bit-counts)]
+    [gamma epsilon]))
+
+(defn get-day03-answer-pt1 []
+  (->> (parse-file)
+       get-gamma-epsilon-bits
+       (map binary-vec->int)
+       (m/ereduce *)))
+
+(defn get-most-popular-bit-at-n [data n]
+  (let [threshold (/ (count data) 2)
+        total (apply + (map #(nth % n) data))]
+    (if (<= threshold total) 1 0)))
+
+(defn matches-bit-at-posn? [l n b]
+  (= b (nth l n)))
+
+(defn get-rating [data popularity]
+  (let [filter-fn (if (= :most-popular popularity)
+                    filter
+                    remove)]
+    (loop [data data
+           n    0]
+      (let [b             (get-most-popular-bit-at-n data n)
+            filtered-data (filter-fn #(matches-bit-at-posn? % n b) data)]
+        (if (= 1 (count filtered-data))
+          (first filtered-data)
+          (recur filtered-data (inc n)))))))
+
+(defn get-day3-answer-pt2 []
+  (let [data (parse-file)]
+    (->> [(get-rating data :most-popular)
+          (get-rating data :least-popular)]
+         (map binary-vec->int)
+         (apply *))))
