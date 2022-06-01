6c81dbac8a1e9c43f665d87ec3a187c88bdc47e9
Susan Luke <susan_luke@mail.com>
2022-06-01 16:21:09 +0100

    tidying

--- src/aoc21/day23.clj
+++ src/aoc21/day23.clj
@@ -1,9 +1,7 @@
 (ns aoc21.day23)
 
-
-
 ;; TODO: sort out all these variables so they can be passed around
-
+;; TODO: delete one grid-info is passed around everywhere
 (def move-costs {:a 1 :b 10 :c 100 :d 1000})
 (def home-cols {:a 3 :b 5 :c 7 :d 9})
 (def home-rows [2 3])
@@ -16,33 +14,16 @@
                 :corridor-row       1
                 :corridor-rest-cols [1 2 4 6 8 10 11]})
 
-;; TODO: do we need a record to represent each amphipod?
-
-;; TODO: Think about whether we should separately keep an list of each amphipod
-;; locations.  I don't think we need to as it's pretty easy to scan through and
-;; find amphis.
-
-
-;; TODO: what is the point of these rows with 13 nils? I
-;; don't think we need them.
-
-
-(def initial-state
-  [(repeat 13 nil)
-   (repeat 13 nil)
-   [nil nil nil :b nil :c nil :b nil :d nil nil nil]
-   [nil nil nil :a nil :d nil :c nil :a nil nil nil]
-   (repeat 13 nil)])
-
-(def grid-num-rows (count initial-state))
-(def grid-num-cols (count (first initial-state)))
+;;(def grid-num-rows (count initial-state))
+;;(def grid-num-cols (count (first initial-state)))
 
 (defn abs [n] (max n (- n)))
 
 (defn get-row-col [m [r c]]
-(-> (nth m r) (nth c)))
+  (-> (nth m r) (nth c)))
 
-(defn all-behind-of-type? [state {:keys [home-rows]} amphi-type [r c]]
+;; TODO: should amphi-type be passed in?  we can infer it.
+(defn all-behind-of-type? [state {:keys [home-rows] :as grid-info} amphi-type [r c]]
   (every? #(= amphi-type
               (get-row-col state [% c]))
           (range (inc r) (inc (apply max home-rows)))))
@@ -57,11 +38,11 @@
 ;;
 ;; Journey down home-col can be calculated separately, taking into account that
 ;; each AP needs to descend a different distance.
-(defn dist-to-get-amphipod-top-of-home-col [state amphi-type [r c :as location]]
+(defn dist-to-get-amphipod-top-of-home-col [state {:keys [home-cols corridor-row] :as grid-info} amphi-type [r c :as location]]
   (let [home-col (home-cols amphi-type)]
     (if (= c home-col)
       ;; Already in home col, now check whether all beneath are the same type
-      (if (all-behind-of-type? state amphi-type location)
+      (if (all-behind-of-type? state grid-info amphi-type location)
         ;; Anything beneath this of the right type, so don't need to move
         0
         ;; Need to move out into corridor, +2 to get to rest location and back
@@ -88,29 +69,29 @@
 (defn home-locations [{:keys [home-rows home-cols] :as grid-info} amphi-type]
   (map vector home-rows (repeat (home-cols amphi-type))))
 
-(defn num-move-in-corridor [state amphi-type]
+(defn num-move-in-corridor [state {:keys [home-rows home-cols] :as grid-info} amphi-type]
   (let [home-col     (home-cols amphi-type)
-        ;; TODO switch to threading macro
-        num-stay-put (-> (take-while #(= % amphi-type)
-                                     (map (partial get-row-col state)
-                                          (reverse (home-locations amphi-type))))
-                         count)]
+        num-stay-put (->> (home-locations grid-info amphi-type)
+                          reverse
+                          (map (partial get-row-col state))
+                          (take-while #(= % amphi-type))
+                          count)]
     (- (count home-rows) num-stay-put)))
 
-(defn dist-to-get-amphipods-home [state amphi-type]
-  (let [num-move-in (num-move-in-corridor state amphi-type)
+(defn dist-to-get-amphipods-home [state grid-info amphi-type]
+  (let [num-move-in (num-move-in-corridor state grid-info amphi-type)
         dist-to-move-in-home-row (apply + (range 1 (inc num-move-in)))]
-    (->> (find-amphipod-locations state amphi-type)
-         (map (partial dist-to-get-amphipod-top-of-home-col state amphi-type))
+    (->> (find-amphipod-locations state grid-info amphi-type)
+         (map (partial dist-to-get-amphipod-top-of-home-col state grid-info amphi-type))
          (apply +)
          (+ dist-to-move-in-home-row))))
 
-(defn cost-to-get-amphipods-home [state amphi-type]
-  (* (amphi-type move-costs)
-     (dist-to-get-amphipods-home state amphi-type)))
+(defn cost-to-get-amphipods-home [state grid-info amphi-type]
+  (* (amphi-type (:move-costs grid-info))
+     (dist-to-get-amphipods-home state grid-info amphi-type)))
 
-(defn cost-to-get-all-amphipods-home [state]
-  (apply + (map (partial cost-to-get-amphipods-home initial-state)
+(defn cost-to-get-all-amphipods-home [state grid-info]
+  (apply + (map (partial cost-to-get-amphipods-home state grid-info)
                 '(:a :b :c :d))))
 
 ;; TODO a bit confusing, should we pass in colum, rather than amphi-type
@@ -153,5 +134,7 @@ at costs for next states and choose the lowest?
 How do we know if we're at a deadend? If no more moves possible?
 
 Check if we're at final state?
+
+NB: Have we thought about costs when amphipod is waiting in the corridor?
 
 ")
--- test/aoc21/day23_test.clj
+++ test/aoc21/day23_test.clj
@@ -2,6 +2,8 @@
   (:require [clojure.test :refer :all]
             [aoc21.day23 :as sut]))
 
+
+;;TODO - we have padding on state that we don't need
 (comment
   "Part 1
 #############
@@ -14,6 +16,11 @@
 (def grid-state-pt1
   [(repeat 13 nil)
    [nil nil nil :b nil :c nil :b nil :d nil nil nil]
+   [nil nil nil :a nil :d nil :c nil :a nil nil nil]])
+
+(def grid-state-pt1-interim
+  [[nil nil :b nil nil nil nil nil nil nil nil :d nil]
+   [nil nil nil nil nil :c nil :b nil nil nil nil nil]
    [nil nil nil :a nil :d nil :c nil :a nil nil nil]])
 
 (def grid-info-pt1 {:num-rows           3
@@ -41,13 +48,9 @@
    [nil nil nil :d nil :b nil :a nil :c nil nil nil]
    [nil nil nil :a nil :d nil :c nil :a nil nil nil]])
 
-(def grid-info-pt2 {:num-rows           5
-                    :num-cols           13
-                    :home-rows          [1 2 3 4]
-                    :home-cols          {:a 3 :b 5 :c 7 :d 9}
-                    :corridor-row       0
-                    :corridor-rest-cols [1 2 4 6 8 10 11]
-                    :move-costs         {:a 1 :b 10 :c 100 :d 1000}})
+(def grid-info-pt2 (-> grid-info-pt1
+                       (assoc :num-rows 5)
+                       (assoc :home-rows [1 2 3 4])))
 
 (deftest get-all-behind-of-type?-test
   ;; Simple test cases
@@ -55,10 +58,12 @@
   (is (= true (sut/all-behind-of-type? [[nil] [:a] [:a] [:a]] {:home-rows [1 2 3]} :a [0 0])))
 
   ;; Pt1 test cases
-  (is (= true (sut/all-behind-of-type? grid-state-pt1 grid-info-pt1 :a [1 3])))
-  (is (= false (sut/all-behind-of-type? grid-state-pt1 grid-info-pt1 :b [1 5])))
-  (is (= true (sut/all-behind-of-type? grid-state-pt1 grid-info-pt1 :c [1 7])))
-  (is (= false (sut/all-behind-of-type? grid-state-pt1 grid-info-pt1 :d [1 9])))
+  (are [tf amphi-type loc]
+      (= tf (sut/all-behind-of-type? grid-state-pt1 grid-info-pt1 amphi-type loc))
+    true :a [1 3]
+    false :b [1 5]
+    true :c [1 7]
+    false :d [1 9])
 
   ;; Pt2 test cases
   (is (= false (sut/all-behind-of-type? grid-state-pt2 grid-info-pt2 :a [1 3])))
@@ -73,43 +78,121 @@
                                            (assoc-in [4 5] :b))
                                        grid-info-pt2 :b [1 5]))))
 
+;; TODO - could complete test cases, but reasonable selection covered
+(deftest dist-to-get-amphipod-top-of-home-col
+  (testing "Part 1 cases"
+    (are
+        [dist amphi-type loc]
+        (= dist (sut/dist-to-get-amphipod-top-of-home-col grid-state-pt1 grid-info-pt1 amphi-type loc))
+
+      8 :a [2 9] ;; up 2 accross 6
+      0 :a [2 3] ;; already in correct position
+      3 :b [1 3] ;; up 1 accross 2
+      3 :b [1 7] ;; up 1 across 2
+      3 :c [1 5] ;; in correct position, but incoreect beneath -> has to move out of the way.  up 1, back and forth 2
+      0 :c [2 7] ;; already in correct position
+      3 :d [1 9] ;; correct posn, but incorrect beneath -> up 1, back and forth 2
+      6 :d [2 5] ;; up 2 accross 2
+      ))
+  (testing "Part 1 interim case"
+    (is (= 3 (sut/dist-to-get-amphipod-top-of-home-col grid-state-pt1-interim grid-info-pt1 :b [0 2])))
+    (is (= 2 (sut/dist-to-get-amphipod-top-of-home-col grid-state-pt1-interim grid-info-pt1 :d [0 11]))))
+  (testing "Part 2 cases"
+    (are
+        [dist amphi-type loc]
+        (= dist (sut/dist-to-get-amphipod-top-of-home-col grid-state-pt2 grid-info-pt2 amphi-type loc))
+
+      8 :a [2 9] ;; up 2 accross 6
+      7 :a [3 7] ;; up 3 across 4
+      0 :a [4 3] ;; already in correct position
+      10 :a [4 9]   ;; up 4 accross 6
+      )))
+
 (deftest find-amphipod-locations-test
   (testing "Part 1 cases"
-    (is (= [[2 3] [2 9]]
-           (sut/find-amphipod-locations grid-state-pt1 grid-info-pt1 :a)))
-    (is (= [[1 3] [1 7]]
-           (sut/find-amphipod-locations grid-state-pt1 grid-info-pt1 :b)))
-    (is (= [[1 5] [2 7]]
-           (sut/find-amphipod-locations grid-state-pt1 grid-info-pt1 :c)))
-    (is (= [[1 9] [2 5]]
-           (sut/find-amphipod-locations grid-state-pt1 grid-info-pt1 :d))))
+    (are [expected amphi-type]
+        (= expected
+           (sut/find-amphipod-locations grid-state-pt1 grid-info-pt1 amphi-type))
+      [[2 3] [2 9]] :a
+      [[1 3] [1 7]] :b
+      [[1 5] [2 7]] :c
+      [[1 9] [2 5]] :d))
 
   (testing "Part 2 cases"
-    (is (= [[2 9] [3 7] [4 3] [4 9]]
-           (sut/find-amphipod-locations grid-state-pt2 grid-info-pt2 :a)))
-    (is (= [[1 3] [1 7] [2 7] [3 5]]
-           (sut/find-amphipod-locations grid-state-pt2 grid-info-pt2 :b)))
-    (is (= [[1 5] [2 5] [3 9] [4 7]]
-           (sut/find-amphipod-locations grid-state-pt2 grid-info-pt2 :c)))
-    (is (= [[1 9] [2 3] [3 3] [4 5]]
-           (sut/find-amphipod-locations grid-state-pt2 grid-info-pt2 :d)))))
+    (are [expected amphi-type]
+        (= expected
+           (sut/find-amphipod-locations grid-state-pt2 grid-info-pt2 amphi-type))
+      [[2 9] [3 7] [4 3] [4 9]] :a
+      [[1 3] [1 7] [2 7] [3 5]] :b
+      [[1 5] [2 5] [3 9] [4 7]] :c
+      [[1 9] [2 3] [3 3] [4 5]] :d)))
 
 (deftest home-locations
   (testing "Part 1 cases"
-    (is (= [[1 3] [2 3]]
-           (sut/home-locations grid-info-pt1 :a)))
-    (is (= [[1 5] [2 5]]
-           (sut/home-locations grid-info-pt1 :b)))
-    (is (= [[1 7] [2 7]]
-           (sut/home-locations grid-info-pt1 :c)))
-    (is (= [[1 9] [2 9]]
-           (sut/home-locations grid-info-pt1 :d))))
+    (are [expected amphi-type]
+        (is (= expected
+               (sut/home-locations grid-info-pt1 amphi-type)))
+      [[1 3] [2 3]] :a
+      [[1 5] [2 5]] :b
+      [[1 7] [2 7]] :c
+      [[1 9] [2 9]] :d))
   (testing "Part 2 cases"
-    (is (= [[1 3] [2 3] [3 3] [4 3]]
-           (sut/home-locations grid-info-pt2 :a)))
-    (is (= [[1 5] [2 5] [3 5] [4 5]]
-           (sut/home-locations grid-info-pt2 :b)))
-    (is (= [[1 7] [2 7] [3 7] [4 7]]
-           (sut/home-locations grid-info-pt2 :c)))
-    (is (= [[1 9] [2 9] [3 9] [4 9]]
-           (sut/home-locations grid-info-pt2 :d)))))
+    (are [expected amphi-type]
+        (is (= expected
+               (sut/home-locations grid-info-pt2 amphi-type)))
+      [[1 3] [2 3] [3 3] [4 3]] :a
+      [[1 5] [2 5] [3 5] [4 5]] :b
+      [[1 7] [2 7] [3 7] [4 7]] :c
+      [[1 9] [2 9] [3 9] [4 9]] :d)))
+
+
+(deftest num-move-in-corridor
+  (are [expected amphi-type]
+      (= expected
+         (sut/num-move-in-corridor grid-state-pt1 grid-info-pt1 amphi-type))
+    1 :a
+    2 :b
+    1 :c
+    2 :d)
+  (are [expected amphi-type]
+      (= expected
+         (sut/num-move-in-corridor grid-state-pt2 grid-info-pt2 amphi-type))
+    3 :a
+    4 :b
+    3 :c
+    4 :d))
+
+(deftest dist-to-get-amphipods-home
+  (testing "Pt1 cases"
+    (are [expected amphi-type]
+        (= expected
+           (sut/dist-to-get-amphipods-home grid-state-pt1 grid-info-pt1 amphi-type))
+      9 :a                  ;; 2U+6L+1D
+      9 :b                  ;; 1U+2L+2D, 1U+2R+1D
+      4 :c                  ;; 1U+2A+1D
+      12 :d                 ;; 1U+1L+1R+2D, 2U+4R+1D
+      ))
+  (testing "Pt2 cases"
+    (are [expected amphi-type]
+        (= expected
+           (sut/dist-to-get-amphipods-home grid-state-pt2 grid-info-pt2 amphi-type))
+      31 :a                 ;; 2U+6L+3D, 3U+4D+2D, 4U+6A+1D = 11+9+11 = 31
+      25 :b                 ;; 1U+2L+4D, 3U+1L+1R+3D, 1U+2L+2D, 2U+2L+1D = 7+8+5+5 = 25
+      18 :c                 ;; 1U+2R+3D, 2U+2R+2D, 3U+2L+1D = 6+6+6 = 18
+      38 :d                 ;; 1U+1R+1L+4D, 2U+6R+3D, 3U+6R+2D, 4U+4R+1D = 7+11+11+9 = 38
+      )))
+
+(deftest cost-to-get-amphipods-home
+  (testing "Pt1 cases"
+    (are [expected amphi-type]
+        (= expected (sut/cost-to-get-amphipods-home grid-state-pt1 grid-info-pt1 amphi-type))
+      9 :a 90 :b 400 :c 12000 :d))
+  (testing "Pt2 cases"
+    (are [expected amphi-type]
+        (= expected (sut/cost-to-get-amphipods-home grid-state-pt2 grid-info-pt2 amphi-type))
+      31 :a 250 :b 1800 :c 38000 :d)))
+
+(deftest cost-to-get-amphipods-home
+  (testing "Pt1"
+    (is (= (+ 9 90 400 12000) (sut/cost-to-get-all-amphipods-home grid-state-pt1 grid-info-pt1)))
+    (is (= (+ 31 250 1800 38000) (sut/cost-to-get-all-amphipods-home grid-state-pt2 grid-info-pt2)))))
